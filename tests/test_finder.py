36fa538dd9d9ebc4267f718b5383ae471429ab59
Claudio Santos <c.santos@consultoriasigma.com.br>
2018-01-24 12:04:39 -0200

    refactoring para testes de pesquisa entenderem onde procurar

--- tests/test_finder.py
+++ tests/test_finder.py
@@ -8,14 +8,14 @@ class TestFinder(TestCase):
 
     def test_must_found_a_subject_mentioned(self):
         fifi_parser = FifiParser()
-        fifi_parser.feed('<html>    <body>      <div class="item">meu item 1            <div>           <a href="link-1">teste</a>          </div>      </div>      <div class="item">meu item 2            <div>               <a href="link-2">teste</a>          </div>      </div>      <div class="item">meu item 3            <div>               <a href="link-3">alvo</a>           </div>      </div>  </body></html>')
+        fifi_parser.feed('<html>    <body>      <div class="item">meu item 1            <div>           <a href="link-1">teste</a>          </div>      </div>      <div class="item">meu item 2            <div>               <a href="link-2">teste</a>          </div>      </div>      <div class="item">meu item 3            <div>               <a href="link-3">alvo</a>           </div>      alvo</div>  </body></html>')
         finder = HtmlFinder(fifi_parser.document)
         result = finder.find_subject('alvo')
         self.assertEqual(1, len(result))
 
     def test_must_found_a_uppercase_subject(self):
         fifi_parser = FifiParser()
-        fifi_parser.feed('<html>    <body>      <div class="item">meu item 1            <div>           <a href="link-1">teste</a>          </div>      </div>      <div class="item">meu item 2            <div>               <a href="link-2">teste</a>          </div>      </div>      <div class="item">meu item 3            <div>               <a href="link-3">alvo</a>           </div>      </div>  </body></html>')
+        fifi_parser.feed('<html>    <body>      <div class="item">            <div>           <a href="link-1">teste</a>          </div>      </div>      <div class="item">meu item 2            <div>               <a href="link-2">teste</a>          </div>      </div>      <div class="item">meu item 3            <div>               <a href="link-3">alvo</a>           </div>     alvo </div>  meu item 1</body></html>')
         finder = HtmlFinder(fifi_parser.document)
         result = finder.find_subject('ALVO')
         self.assertEqual(1, len(result))
@@ -29,14 +29,14 @@ class TestFinder(TestCase):
 
     def test_must_found_a_complex_subject(self):
         fifi_parser = FifiParser()
-        fifi_parser.feed('<html>    <body>      <div class="item">meu item 1            <div>           <a href="link-1">teste</a>          </div>      </div>      <div class="item">meu item 2            <div>               <a href="link-2">teste</a>          </div>      </div>      <div class="item">meu item 3            <div>               <a href="link-3">Alvo Existente</a>           </div>      </div>  </body></html>')
+        fifi_parser.feed('<html>    <body>      <div class="item">meu item 1            <div>           <a href="link-1">teste</a>          </div>      </div>      <div class="item">meu item 2            <div>               <a href="link-2">teste</a>          </div>      </div>      <div class="item">meu item 3            <div>               <a href="link-3">Alvo Existente</a>           </div>Alvo Existente      </div>  </body></html>')
         finder = HtmlFinder(fifi_parser.document)
         result = finder.find_subject('Alvo Existente')
         self.assertEqual(1, len(result))
 
     def test_must_found_more_than_one_subject(self):
         fifi_parser = FifiParser()
-        fifi_parser.feed('<html>    <body>      <div class="item">meu item 1            <div>           <a href="link-1">teste</a>          </div>      </div>      <div class="item">meu item 2            <div>               <a href="link-2">Alvo Existente</a>          </div>      </div>      <div class="item">meu item 3            <div>               <a href="link-3">Alvo Existente</a>           </div>      </div>  </body></html>')
+        fifi_parser.feed('<html>    <body>      <div class="item">meu item 1            <div>           <a href="link-1">teste</a>          </div>      </div>      <div class="item">meu item 2            <div>               <a href="link-2">Alvo Existente</a>          </div>Alvo Existente      </div>      <div class="item">meu item 3            <div>               <a href="link-3">Alvo Existente</a>           </div>Alvo Existente      </div>  </body></html>')
         finder = HtmlFinder(fifi_parser.document)
         result = finder.find_subject('Alvo Existente')
         self.assertEqual(2, len(result))
